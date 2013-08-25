d63114464d20751e4d38417af4ff5fbdef5c372e
Joshua Bell <pensivearchitect@gmail.com>
2013-08-25 15:18:04 -0700

    rails new dgswebsite --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dgswebsite::Application.config.secret_key_base = 'a9a8f74ef02c942c20157fe36606b5a5103fa6fbe005cc378d9335360902c7fbc22a030dcfa44d2be96eb2f3ed65512ca3dc7a989c039a72a2507fd163864262'
