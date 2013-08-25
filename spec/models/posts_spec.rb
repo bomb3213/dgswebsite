10091f2cec57f455f18e555565aa19170399ad51
Joshua Bell <pensivearchitect@gmail.com>
2013-08-25 16:35:24 -0700

    Added basic tests to posts_spec

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Posts do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should have_many(:tags).through(:taggings_ }
+
 end
